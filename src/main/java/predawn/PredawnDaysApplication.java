abb27a78608236a84ef4b96f74a833b24c0bca86
moongs <jinee5050@naver.com>
2026-04-07 03:49:05 +0900

    first commit

---
+++ src/main/java/predawn/PredawnDaysApplication.java
@@ -0,0 +1,13 @@
+package predawn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredawnDaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredawnDaysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/predawn/PredawnDaysApplicationTests.java
@@ -0,0 +1,13 @@
+package project.predawn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredawnDaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
