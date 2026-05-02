b6ebfbffeeb2d7acb7abc114efdd4296e18fa269
moongs <jinee5050@naver.com>
2026-05-02 14:49:43 +0900

    [DB] schema.sql 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE upload_file (
+CREATE TABLE IF NOT EXISTS upload_file (
 	upload_file_id BIGINT NOT NULL AUTO_INCREMENT,
     upload_file_name VARCHAR(100) NOT NULL,
     storage_key VARCHAR(200) NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE upload_file (
     PRIMARY KEY (upload_file_id)
 );
 
-CREATE TABLE member (
+CREATE TABLE IF NOT EXISTS member (
 	member_id BIGINT NOT NULL AUTO_INCREMENT,
     login_id VARCHAR(30) NOT NULL,
     password varchar(30) NOT NULL,
@@ -29,7 +29,28 @@ CREATE TABLE member (
 		REFERENCES upload_file(upload_file_id)
 );
 
-CREATE TABLE category (
+CREATE TABLE IF NOT EXISTS friend (
+	friend_id BIGINT NOT NULL AUTO_INCREMENT,
+    member_id BIGINT NOT NULL,
+    friend_member_id BIGINT NOT NULL,
+    request_id BIGINT NOT NULL,
+    status VARCHAR(30) NOT NULL,
+    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    last_modified_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (friend_id),
+    CONSTRAINT uq_member_id_friend_member_id UNIQUE (member_id, friend_member_id),
+    CONSTRAINT ck_member_id_friend_member_id CHECK (member_id < friend_member_id),
+    CONSTRAINT ck_request_id CHECK (request_id IN (member_id, friend_member_id)),
+    CONSTRAINT fk_friend_member FOREIGN KEY (member_id)
+		REFERENCES member(member_id),
+    CONSTRAINT fk_friend_friend_member FOREIGN KEY (friend_member_id)
+        REFERENCES member(member_id),
+    CONSTRAINT fk_friend_request FOREIGN KEY (request_id)
+        REFERENCES member(member_id)
+);
+
+CREATE TABLE IF NOT EXISTS category (
 	category_id BIGINT NOT NULL AUTO_INCREMENT,
     name VARCHAR(20) NOT NULL,
 	created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -38,7 +59,7 @@ CREATE TABLE category (
 	PRIMARY KEY (category_id)
 );
 
-CREATE TABLE board (
+CREATE TABLE IF NOT EXISTS board (
 	board_id BIGINT NOT NULL AUTO_INCREMENT,
     member_id BIGINT NOT NULL,
     title VARCHAR(50) NOT NULL,
@@ -60,7 +81,7 @@ CREATE TABLE board (
 );
 
 
-CREATE TABLE board_attach_file (
+CREATE TABLE IF NOT EXISTS board_attach_file (
 	board_attach_file_id BIGINT NOT NULL AUTO_INCREMENT,
 	board_id BIGINT NOT NULL,
     upload_file_id BIGINT NOT NULL,
@@ -76,7 +97,7 @@ CREATE TABLE board_attach_file (
 		REFERENCES upload_file(upload_file_id)
 );
 
-CREATE TABLE comment (
+CREATE TABLE IF NOT EXISTS comment (
 	comment_id BIGINT NOT NULL AUTO_INCREMENT,
     member_id BIGINT NOT NULL,
     board_id BIGINT NOT NULL,
@@ -91,7 +112,7 @@ CREATE TABLE comment (
 		REFERENCES board (board_id)
 );
 
-CREATE TABLE reply (
+CREATE TABLE IF NOT EXISTS reply (
 	reply_id BIGINT NOT NULL AUTO_INCREMENT,
     member_id BIGINT NOT NULL,
     comment_id BIGINT NOT NULL,
@@ -106,7 +127,7 @@ CREATE TABLE reply (
 		REFERENCES comment (comment_id)
 );
 
-CREATE TABLE board_view_history (
+CREATE TABLE IF NOT EXISTS board_view_history (
 	board_view_history_id BIGINT NOT NULL AUTO_INCREMENT,
     member_id BIGINT NOT NULL,
     board_id BIGINT NOT NULL,
@@ -123,7 +144,7 @@ CREATE TABLE board_view_history (
 );
 
 
-CREATE TABLE likes (
+CREATE TABLE IF NOT EXISTS likes (
 	like_id BIGINT NOT NULL AUTO_INCREMENT,
     member_id BIGINT NOT NULL,
 	board_id BIGINT NOT NULL,
@@ -138,7 +159,7 @@ CREATE TABLE likes (
 		REFERENCES board(board_id)
 );
 
-CREATE TABLE shedlock (
+CREATE TABLE IF NOT EXISTS shedlock (
 	name VARCHAR(100) NOT NULL,
     lock_until TIMESTAMP NOT NULL,
     locked_at TIMESTAMP NOT NULL,
@@ -146,7 +167,7 @@ CREATE TABLE shedlock (
     primary key (name)
 );
 
-CREATE TABLE spring_session (
+CREATE TABLE IF NOT EXISTS spring_session (
     primary_id CHAR(36) NOT NULL,
     session_id CHAR(36) NOT NULL,
     creation_time BIGINT NOT NULL,
@@ -155,14 +176,14 @@ CREATE TABLE spring_session (
     expiry_time BIGINT NOT NULL,
     principal_name VARCHAR(100),
 
-    PRIMARY KEY (primary_id)
+    PRIMARY KEY (primary_id),
+    CONSTRAINT idx_spring_session_id UNIQUE (session_id)
 );
 
-CREATE UNIQUE INDEX idx_spring_session_id ON spring_session (session_id);
-CREATE INDEX idx_spring_session_expiry_time ON spring_session (expiry_time);
-CREATE INDEX idx_spring_session_principal_name ON spring_session (principal_name);
+CREATE INDEX IF NOT EXISTS idx_spring_session_expiry_time ON spring_session (expiry_time);
+CREATE INDEX IF NOT EXISTS idx_spring_session_principal_name ON spring_session (principal_name);
 
-CREATE TABLE spring_session_attributes (
+CREATE TABLE IF NOT EXISTS spring_session_attributes (
     session_primary_id CHAR(36) NOT NULL,
     attribute_name VARCHAR(200) NOT NULL,
     attribute_bytes BLOB NOT NULL,
